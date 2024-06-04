915ee43e9f216c8ea91da1639c270435bef4d6a9
Vladimir <jaln.102406@gmail.com>
2024-06-04 09:44:48 +0300

    v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import { MainPage } from 'pages/MainPage';
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <MainPage />
+  <QueryClientProvider client={queryClient}>
+    <MainPage />
+  </QueryClientProvider>
 )
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import useWebSocket from 'react-use-websocket';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc'
@@ -10,16 +10,47 @@ import { Button, Card, Flex, Form, Input, Space } from 'antd';
 import { SettingOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
+
+import cls from './MainPage.module.scss'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
 dayjs.locale('ru')
 
 
 export const MainPage = () => {
+  const queryClient = useQueryClient()
   const [messages, setMessages] = useState([]);
-  const [proxies, setProxies] = useState([]);
-  const [config, setConfig] = useState({});
+  const { data: config, isLoading } = useQuery({
+    queryKey: ['config'],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `${API_URL}/config`
+      )
+      return data
+    }
+  });
+  const pauseMutation = useMutation({
+    mutationFn: (data) => axios.put(API_URL + '/config/1', data),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['config'] })
+  })
+  const configMutation = useMutation({
+    mutationFn: (data) => axios.put(API_URL + '/config/1', data),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['config'] })
+  })
+  const { data: proxies, isLoading: isProxyLoading } = useQuery({
+    queryKey: ['proxies'],
+    queryFn: async () => {
+      const { data } = await axios.get(`${API_URL}/proxies/`)
+      return data
+    }
+  });
+  const proxiesMutation = useMutation({
+    mutationFn: (data: { id: number, body: Record<string, string> }) => axios.put(`${API_URL}/proxies/${data?.id}`, data?.body),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['proxies'] })
+  })
   const [settings, setSettings] = useState(false);
-  const WS_URL = 'ws://localhost:8000'
-  const API_URL = 'http://localhost:8000'
+  const WS_URL = 'ws://168.100.8.246:8000/'
+  const API_URL = 'http://168.100.8.246:8000'
   const regex = /^http:\/\/([^:]+:[^@]+)@([^:]+:[^/]+)/
 
   const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
@@ -30,41 +61,36 @@ export const MainPage = () => {
   )
 
   const handlePause = () => {
-    axios.put(API_URL + '/config/1', {
+    pauseMutation.mutate({
       ...config,
       pause: !config?.pause
     })
   }
 
   const handleChange = (values: any) => {
-    axios.put(API_URL + '/config/1', {
+    configMutation.mutate({
       ...values,
       pause: config?.pause
     })
   }
 
   const handleChangeProxy = (values: any, id: number) => {
-    axios.put(API_URL + `/proxies/${id}`, {
-      proxy_id: values.proxy_id,
-      url: `http://${values['login:password']}@${values['url:port']}`,
-      change_ip: values.change_ip
+    proxiesMutation.mutate({
+      id: id,
+      body: {
+        proxy_id: values.proxy_id,
+        url: `http://${values['login:password']}@${values['url:port']}`,
+        change_ip: values.change_ip
+      }
     })
   }
 
-  useEffect(() => {
-    axios.get(API_URL + '/config').then((res) => {
-      setConfig(res.data)
-    })
-      .catch((err) => console.log(err));
-  }, [setConfig])
-
-  useEffect(() => {
-    axios.get(API_URL + '/proxies/').then((res) => {
-      setProxies(res.data)
-    })
-      .catch((err) => console.log(err));
-  }, [setProxies])
-
+  const configClicks = useMemo(() => {
+    return {
+      clicks: lastJsonMessage?.clicks,
+      allClicks: lastJsonMessage?.allClicks
+    }
+  }, [lastJsonMessage])
 
   useEffect(() => {
     console.log(lastJsonMessage)
@@ -73,20 +99,19 @@ export const MainPage = () => {
     }
   }, [lastJsonMessage])
 
+  if (isLoading || isProxyLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <Flex style={{
-      width: '100%',
-      height: '100vh',
-      backgroundColor: '#f5f5f5',
-      padding: '30px 0 30px 30px'
-    }} align='center' justify='space-around' vertical>
-      <Card style={{ width: '90%', maxHeight: '70vh' }}>
-        <Flex align='center' justify='space-around'>
+    <Flex className={cls.mainPage} align='center' justify='space-around' vertical gap={10}>
+      <Card style={{ width: '90%', maxHeight: '70vh', overflowY: 'auto' }}>
+        <div className={cls.header}>
           <span>Info: </span>
-          <span>Количество кликов: 0/1800</span>
+          <span>Количество кликов: {configClicks.clicks ?? 0}/{configClicks.allClicks ?? 0}</span>
           {config.pause ? <Button onClick={handlePause} type='primary'>Возобновить работу</Button> : <Button onClick={handlePause} danger>Остановить работу</Button>}
           <Button onClick={() => setSettings((prev) => !prev)}><SettingOutlined /></Button>
-        </Flex>
+        </div>
         {settings && (
           <Flex style={{ marginTop: 15 }} vertical gap='14px'>
             <Flex align='center' justify='center'>
@@ -96,11 +121,11 @@ export const MainPage = () => {
                 <Form.Item><Button htmlType='submit' type='primary'>Сохранить</Button></Form.Item>
               </Form>
             </Flex>
-            <Flex align='center' justify='space-around'>
+            <div className={cls.formWrapper}>
               <Form onFinish={(values) => handleChangeProxy(values, 1)} initialValues={{
                 proxy_id: proxies[0].proxy_id,
-                'url:port': proxies[0].url.match(regex)[1],
-                'login:password': proxies[0].url.match(regex)[2],
+                'url:port': proxies[0].url.match(regex)[2],
+                'login:password': proxies[0].url.match(regex)[1],
                 change_ip: proxies[0].change_ip
               }} name='proxy1'>
                 <Form.Item name='proxy_id' label='Proxy id'>
@@ -122,12 +147,14 @@ export const MainPage = () => {
                   </Space>
                 </Form.Item>
               </Form>
-              <Form onFinish={(values) => handleChangeProxy(values, 2)} initialValues={{
-                proxy_id: proxies[1].proxy_id,
-                'url:port': proxies[1].url.match(regex)[1],
-                'login:password': proxies[1].url.match(regex)[2],
-                change_ip: proxies[1].change_ip
-              }} name='proxy2'>
+              <Form onFinish={(values) => handleChangeProxy(values, 2)}
+                initialValues={proxies[1] ? {
+                  proxy_id: proxies[1].proxy_id,
+                  'url:port': proxies[1].url.match(regex)[2],
+                  'login:password': proxies[1].url.match(regex)[1],
+                  change_ip: proxies[1].change_ip
+                } : {}}
+                name='proxy2'>
                 <Form.Item name='proxy_id' label='Proxy id'>
                   <Input />
                 </Form.Item>
@@ -147,12 +174,14 @@ export const MainPage = () => {
                   </Space>
                 </Form.Item>
               </Form>
-              <Form onFinish={(values) => handleChangeProxy(values, 3)} initialValues={{
-                proxy_id: proxies[2].proxy_id,
-                'url:port': proxies[2].url.match(regex)[1],
-                'login:password': proxies[2].url.match(regex)[2],
-                change_ip: proxies[2].change_ip
-              }} name='proxy3'>
+              <Form onFinish={(values) => handleChangeProxy(values, 3)}
+                initialValues={proxies[2] ? {
+                  proxy_id: proxies[2].proxy_id,
+                  'url:port': proxies[2].url.match(regex)[2],
+                  'login:password': proxies[2].url.match(regex)[1],
+                  change_ip: proxies[2].change_ip
+                } : {}}
+                name='proxy3'>
                 <Form.Item name='proxy_id' label='Proxy id'>
                   <Input />
                 </Form.Item>
@@ -172,12 +201,14 @@ export const MainPage = () => {
                   </Space>
                 </Form.Item>
               </Form>
-              <Form onFinish={(values) => handleChangeProxy(values, 4)} initialValues={{
-                proxy_id: proxies[3].proxy_id,
-                'url:port': proxies[3].url.match(regex)[1],
-                'login:password': proxies[3].url.match(regex)[2],
-                change_ip: proxies[3].change_ip
-              }} name='proxy4'>
+              <Form onFinish={(values) => handleChangeProxy(values, 4)}
+                initialValues={proxies[3] ? {
+                  proxy_id: proxies[3].proxy_id,
+                  'url:port': proxies[3].url.match(regex)[2],
+                  'login:password': proxies[3].url.match(regex)[1],
+                  change_ip: proxies[3].change_ip
+                } : {}}
+                name='proxy4'>
                 <Form.Item name='proxy_id' label='Proxy id'>
                   <Input />
                 </Form.Item>
@@ -197,12 +228,14 @@ export const MainPage = () => {
                   </Space>
                 </Form.Item>
               </Form>
-              <Form onFinish={(values) => handleChangeProxy(values, 5)} initialValues={{
-                proxy_id: proxies[4].proxy_id,
-                'url:port': proxies[4].url.match(regex)[1],
-                'login:password': proxies[4].url.match(regex)[2],
-                change_ip: proxies[4].change_ip
-              }} name='proxy5'>
+              <Form onFinish={(values) => handleChangeProxy(values, 5)}
+                initialValues={proxies[4] ? {
+                  proxy_id: proxies[4].proxy_id,
+                  'url:port': proxies[4].url.match(regex)[2],
+                  'login:password': proxies[4].url.match(regex)[1],
+                  change_ip: proxies[4].change_ip
+                } : {}}
+                name='proxy5'>
                 <Form.Item name='proxy_id' label='Proxy id'>
                   <Input />
                 </Form.Item>
@@ -222,15 +255,15 @@ export const MainPage = () => {
                   </Space>
                 </Form.Item>
               </Form>
-            </Flex>
+            </div>
           </Flex>
         )
         }
       </Card>
-      <Card style={{ width: '90%', height: '70vh' }}>
+      <Card style={{ width: '90%', height: '70vh', overflowY: 'auto' }}>
         {messages.map((message, idx) => (
           <div key={idx}>
-            <p>Город - {message.city}, сделанно кликов - {message.clicks}/{message.allClicks} | {dayjs(message.time).locale('ru').format('DD/MM/YYYY HH:mm:ss')}</p>
+            <p>Город - {message.city}, сделанно кликов - {message.cityClicks}/{message.allCityClicks} | {dayjs(message.time).locale('ru').format('DD/MM/YYYY HH:mm:ss')}</p>
           </div>
         ))}
       </Card>
